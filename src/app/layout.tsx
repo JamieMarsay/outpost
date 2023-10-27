9332ad0ea86238b1e6b05e2a1810bb05ddc3727d
Jamie Marsay <jamiemarsay@jamies-air.lan>
2023-10-27 14:13:15 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import "./globals.css";
 // import Header from "../components/Header/Header";
 // import Footer from "../components/Footer/Footer";
-import { GameProvider } from "../context/Game/GameProvider";
-import { QuizEngineProvider } from "../context/QuizEngine/QuizEngineProvider";
+// import { GameProvider } from "../context/Game/GameProvider";
+// import { QuizEngineProvider } from "../context/QuizEngine/QuizEngineProvider";
 
 export default function RootLayout({
   children,
@@ -14,7 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=ABeeZee&display=swap"
           rel="stylesheet"
